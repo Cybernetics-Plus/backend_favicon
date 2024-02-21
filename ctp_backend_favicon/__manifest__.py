517759ebbe9d16612418eef44ec7f1f3633aec40
BOMEN <annop.c@cybernetics.plus>
2024-02-21 21:35:47 +0700

    Start Module
    Start Module

---
+++ ctp_backend_favicon/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    Cybernetics Plus Co., Ltd.
+#    C+ Backend Favicon
+#
+#    Copyright (C) 2021-TODAY Cybernetics Plus Technologies (<https://www.cybernetics.plus>).
+#    Author: Cybernetics Plus Techno Solutions (<https://www.cybernetics.plus>)
+#
+###################################################################################
+
+{
+    "name": "Backend Favicon",
+    "version": "17.0.1.0.1",
+    "summary": "Backend Favicon",
+    "description": "Backend Favicon",
+    "author": "Cybernetics+",
+    "website": "https://www.cybernetics.plus",
+    "live_test_url": "https://www.cybernetics.plus",
+    "category": "Customizations",
+    "license": "Other proprietary",
+    "price": 9.99,
+    "currency": "EUR",
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "contributors": [
+        "C+ Developer <dev@cybernetics.plus>",
+    ],
+    "depends": ["base"],
+    "data": [
+        "views/web_layout.xml",
+    ],
+}
